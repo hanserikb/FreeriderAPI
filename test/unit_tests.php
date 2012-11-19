007fdc0a500042ddc992026bf690b788e747c597
Hans Bentlöv <hanserikb@gmail.com>
2012-11-19 12:25:11 +0100

    Unit tests

--- test/unit_tests.php
+++ test/unit_tests.php
@@ -1,8 +1,36 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: HansBentlov
- * Date: 2012-11-19
- * Time: 11:57
- * To change this template use File | Settings | File Templates.
- */
+
+require_once("../../simpletest/autorun.php");
+require_once("../Freerider.php");
+class FreeriderAPITEsts extends UnitTestCase
+{
+
+    function testFreeriderObject()
+    {
+        $freerider = new Freerider("Stockholm", "Västerås", date("Y-m-d", strtotime("2012-11-18")), date("Y-m-d", strtotime("2012-11-20")), "Volvo V40");
+        $this->assertTrue(1 == 1);
+    }
+
+
+    function testGetAll()
+    {
+        // Gör ett anrop
+        // En array med minst ett objekt
+        $result = null;
+      //  $this->assertTrue(count($result) > 1, "Keyword search failed, no results");
+    }
+
+    function testGetDestination()
+    {
+        // Gör ett anrop med "Stockholm" som argument
+
+        // Få tillbaka minst ett resultat med "Stockholm" som destination
+    }
+
+    function testGetOrigin()
+    {
+        // Gör ett anrop med "Stockholm" som argument
+
+        // Få tillbaka minst ett resultat innehållandes "Stockholm" som startpunkt
+    }
+}
