82e6268e0571321547be4870139075408cca1185
Hans Bentlöv <hanserikb@gmail.com>
2012-11-19 12:24:21 +0100

    Add gitignore

---
+++ Freerider.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: HansBentlov
+ * Date: 2012-11-19
+ * Time: 12:03
+ * To change this template use File | Settings | File Templates.
+ */
+class Freerider
+{
+
+}
---
+++ test/unit_tests.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: HansBentlov
+ * Date: 2012-11-19
+ * Time: 11:57
+ * To change this template use File | Settings | File Templates.
+ */
